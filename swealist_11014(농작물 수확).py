1d02699614394a820e398e5471349530fa52bd80
ejuun <wndjs8225@gmail.com>
2023-02-26 23:41:12 +0900

    0226 crop solve

--- swealist_11014(농작물 수확).py
+++ swealist_11014(농작물 수확).py
@@ -3,31 +3,34 @@ for tc in range(1, T + 1):
     N = int(input())
     land = [list(map(int, input().split())) for _ in range(N)]
 
-    # ㅏ 형식
-    min_sum = 100000000
+    min_val = 1000000
+    #1열 ~ 마지막 열까지 진행
+    for a in range(1, N):
+        #세로합
+        sero = 0
+        for i in range(N):
+            for j in range(a, N):
+                sero += land[i][j]
 
-    for i in range(1, N):
-        for j in range(1, N):
-            sero = 0
+        #가로 합 구하기
+        for c in range(1, N):
             garo_up = 0
             garo_down = 0
-            sec_sum = 0
-            # 세로 합
-            for s in range(N):
-                for e in range(j):
-                    sero += land[s][e]
-            # 가로 위 합
-            for u in range(i):
-                for p in range(j, N):
-                    garo_up += land[u][p]
-            # 가로 아래 합
-            for d in range(i, N):
-                for o in range(j, N):
-                    garo_down += land[d][o]
 
+            # 가로 윗 부분의 합 구하기
+            for u in range(c):
+                for g in range(a):
+                    garo_up += land[u][g]
+
+            #가로 아랫 부분의 합 구하기
+            for d in range(N-c):
+                for g in range(a):
+                    garo_down += land[c+d][g]
+
+            #농작물 총합의 최대값과 최솟값의 차이
             sec_sum = max(sero, garo_up, garo_down) - min(sero, garo_up, garo_down)
 
-            if min_sum > sec_sum:
-                min_sum = sec_sum
+            if min_val > sec_sum:
+                min_val = sec_sum
 
-    print(f'#{tc} {min_sum}')
+    print(f'#{tc} {min_val}')
